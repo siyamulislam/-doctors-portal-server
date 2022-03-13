52aa35a96a0705694e78d8466daf419c5f07df53
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-03-14 00:19:33 +0600

    image direct uplaod mongo

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
 client.connect(err => {
     const appointmentsCollections = client.db("doctorsPortal").collection("appointments");
     const doctorCollection = client.db("doctorsPortal").collection("doctors");
@@ -50,38 +49,58 @@ client.connect(err => {
                     })
             })
     })
+
     app.post('/addADoctor', (req, res) => {
         const file = req.files.file;
         const name = req.body.name;
         const email = req.body.email;
         const phone = req.body.phone;
-        const filePath = `${__dirname}/doctors/${file.name}`
         console.log(file);
-        file.mv(filePath, err => {
-            if (err) {
-                console.log(error);
-                return res.status(500).send({ mess: 'failed to upload image to Server' })
-            }
-            const newImage = fs.readFileSync(filePath);
-            const encImage = newImage.toString('base64');
-            var image = {
-                contentType: req.files.file.mimetype,
-                size: req.files.file.size,
-                img: Buffer(encImage, 'base64')
-            }
-            doctorCollection.insertOne({ name, email, phone, image })
-                .then(result => {
-                    fs.remove(filePath, err => {
-                        if (err) { console.log(err);
-                            res.status(500).send({ mess: 'failed to upload image to Server' })
-                        }
-                        res.send(result.acknowledged);
-                    })
-
-                })
-            // return res.send({name:file.name,path:`/${file.name}`})
-        })
+        const newImage = file.data;
+        const encImage = newImage.toString('base64');
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImage, 'base64')
+        }
+        doctorCollection.insertOne({ name, email, phone, image })
+            .then(result => {
+                res.send(result.acknowledged);
+            })
     })
+
+
+    // doctor folder to mongo db
+    // app.post('/addADoctor', (req, res) => {
+    //     const file = req.files.file;
+    //     const name = req.body.name;
+    //     const email = req.body.email;
+    //     const phone = req.body.phone;
+    //     const filePath = `${__dirname}/doctors/${file.name}`
+    //     console.log(file);
+    //     file.mv(filePath, err => {
+    //         if (err) {
+    //             console.log(error);
+    //             return res.status(500).send({ mess: 'failed to upload image to Server' })
+    //         }  
+    //         const newImage = fs.readFileSync(filePath);
+    //         const encImage = newImage.toString('base64');
+    //         var image = {
+    //             contentType: req.files.file.mimetype,
+    //             size: req.files.file.size,
+    //             img: Buffer(encImage, 'base64')
+    //         }
+    //         doctorCollection.insertOne({ name, email, phone, image })
+    //             .then(result => {
+    //                 fs.remove(filePath, err => {
+    //                     if (err) { console.log(err);
+    //                         res.status(500).send({ mess: 'failed to upload image to Server' })
+    //                     }
+    //                     res.send(result.acknowledged);
+    //                 })
+    //             })
+    //     })
+    // })
 
 
     app.get('/doctors', (req, res) => {
@@ -99,30 +118,7 @@ client.connect(err => {
             })
     })
 
-    //  //to show products on ReviewItem.js
-    // app.get('/product/:key', (req, res) => {
-    //     productsCollections.find({ key: req.params.key })
-    //         .toArray((err, documents) => {
-    //             res.send(documents[0])
-    //         })
-    // })
-    //  //to show products on Review.js and Shop.js
-    // app.post('/productsByKey', (req, res) => {
-    //     const productKeys = req.body;
-    //     productsCollections.find({ key: { $in: productKeys } })
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
-    //  //to process product order on Shipment.js
-    // app.post('/addOrder', (req, res) => {
-    //     const order = req.body;
-    //     ordersCollections.insertOne(order)
-    //         .then(result => {
-    //             console.log(result);
-    //             res.send(result.acknowledged)
-    //         })
-    // })
+
 
     console.log('db connected');
 });
